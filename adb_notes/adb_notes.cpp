544785b6699360c0a64060e965a3ee1e85fab53e
Yyyyshen <Yyyyshen@gmail.com>
2021-12-08 14:01:11 +0800

    adb常用命令

---
+++ adb_notes/adb_notes.cpp
@@ -0,0 +1,20 @@
+﻿// adb_notes.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+//
+//adb常用命令
+//
+
+//
+//获取APK
+//adb shell pm list package
+//adb shell pm path com.DeviceTes
+//adb pull /system/priv-app/DeviceTest/DeviceTest.apk  C:\Users\Administrator\Desktop\
+//
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
